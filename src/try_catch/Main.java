691f35c7620efd59b6132e13eb4b1f0f3a681fb9
yoihenken <18104005@ittelkom-pwt.ac.id>
2020-01-09 15:31:01 +0700

    Input with Console class, with MAC path

--- src/try_catch/Main.java
+++ src/try_catch/Main.java
@@ -5,11 +5,10 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class Main {
-
     public static void main(String[] args) {
         try{
             //Mencoba membaca berkas latihan.txt
-            File file = new File("D://latihan.txt");
+            File file = new File("/Users/yoihenken/Documents/data.txt");
             FileReader fr = new FileReader(file);
             //Jika berhasil
             System.out.println("Read file berhasil");
